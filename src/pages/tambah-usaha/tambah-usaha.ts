45e62be504a16c7bf98bed728ca5dd624403f914
vitorizkiimanda <vitorizkiimanda@gmail.com>
2018-02-11 17:09:45 +0700

    camera - tambah usaha

--- src/pages/tambah-usaha/tambah-usaha.ts
+++ src/pages/tambah-usaha/tambah-usaha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ActionSheetController } from 'ionic-angular/components/action-sheet/action-sheet-controller';
 /**
  * Generated class for the TambahUsahaPage page.
  *
@@ -15,16 +16,83 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class TambahUsahaPage {
 
   submitted = false;
+  image:any;
 
-  constructor(private camera: Camera, public navCtrl: NavController, public navParams: NavParams) {
+  validPhoto= false;
+
+  constructor(
+    private camera: Camera, 
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public actionSheetCtrl : ActionSheetController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TambahUsahaPage');
   }
 
-  takePhoto(){
-    
+  updatePicture() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Pilihan',
+      buttons: [
+        {
+          text: 'Ambil Gambar Baru',
+          role: 'ambilGambar',
+          handler: () => {
+            this.takePicture();
+          }
+        },
+        {
+          text: 'Pilih Dari Galleri',
+          role: 'gallery',
+          handler: () => {
+            this.getPhotoFromGallery();
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  async takePicture(){
+    try {
+      const options : CameraOptions = {
+        quality: 50, //to reduce img size
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL, //to make it base64 image
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType:this.camera.MediaType.PICTURE,
+        correctOrientation: true
+      }
+
+      const result =  await this.camera.getPicture(options);
+
+      this.image = 'data:image/jpeg;base64,' + result;
+
+      this.validPhoto=true;
+
+    }
+    catch (e) {
+      console.error(e);
+      alert("error");
+    }
+
+  }
+
+  getPhotoFromGallery(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType     : this.camera.PictureSourceType.PHOTOLIBRARY,
+        targetWidth: 600,
+        targetHeight: 600
+    }).then((imageData) => {
+      // this.base64Image = imageData;
+      // this.uploadFoto();
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      this.validPhoto=true;
+      }, (err) => {
+    });
   }
 
 }
