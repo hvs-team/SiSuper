24549ecf4a1817e70b9de5d144235cb0b523c22a
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-04-13 11:26:50 +0700

    camera added to umkm profile page

--- src/pages/profile-usaha/profile-usaha.ts
+++ src/pages/profile-usaha/profile-usaha.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ActionSheetController } from 'ionic-angular';
 import { EditProfileUsahaPage } from '../edit-profile-usaha/edit-profile-usaha';
 import { ListUsahaPage } from '../list-usaha/list-usaha';
 
+
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-profile-usaha',
@@ -10,8 +12,16 @@ import { ListUsahaPage } from '../list-usaha/list-usaha';
 })
 export class ProfileUsahaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-    public alertCtrl: AlertController) {
+  image:any;
+
+  validPhoto= false;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public alertCtrl: AlertController,
+    private camera: Camera,
+    public actionSheetCtrl : ActionSheetController,) {
   }
 
   ionViewDidLoad() {
@@ -61,12 +71,76 @@ export class ProfileUsahaPage {
           text: 'Perharui',
           handler: () => {
             console.log('Agree clicked');
-            // this.updatePicture(); 
+            this.updatePicture(); 
           }
         }
       ]
     });
       confirm.present();
+  }
+
+  updatePicture() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Pilihan',
+      buttons: [
+        {
+          text: 'Ambil Gambar Baru',
+          role: 'ambilGambar',
+          handler: () => {
+            this.takePicture();
+          }
+        },
+        {
+          text: 'Pilih Dari Galleri',
+          role: 'gallery',
+          handler: () => {
+            this.getPhotoFromGallery();
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  async takePicture(){
+    try {
+      const options : CameraOptions = {
+        quality: 50, //to reduce img size
+        targetHeight: 600,
+        targetWidth: 600,
+        destinationType: this.camera.DestinationType.DATA_URL, //to make it base64 image
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType:this.camera.MediaType.PICTURE,
+        correctOrientation: true
+      }
+
+      const result =  await this.camera.getPicture(options);
+
+      this.image = 'data:image/jpeg;base64,' + result;
+
+      this.validPhoto=true;
+
+    }
+    catch (e) {
+      console.error(e);
+      alert("error");
+    }
+
+  }
+
+  getPhotoFromGallery(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType     : this.camera.PictureSourceType.PHOTOLIBRARY,
+        targetWidth: 600,
+        targetHeight: 600
+    }).then((imageData) => {
+      // this.base64Image = imageData;
+      // this.uploadFoto();
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      this.validPhoto=true;
+      }, (err) => {
+    });
   }
 
 
